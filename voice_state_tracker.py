0140c38a80e28e57fa1197e81cf97a395db4807a
yukinoshita023 <tm.lovin8@gmail.com>
2026-06-10 22:47:27 +0900

    #20 移譲コマンドを削除

--- voice_state_tracker.py
+++ voice_state_tracker.py
@@ -40,7 +40,7 @@ async def handle_voice_state_update(member: discord.Member, before, after):
 async def grant_points_loop(user_id):
     try:
         while user_id in user_states:
-            await asyncio.sleep(300)  # 5分
+            await asyncio.sleep(3600)  # 60分
 
             category_id = user_states[user_id]["category_id"]
             category_name = CATEGORY_VC_MAPPING[category_id]
